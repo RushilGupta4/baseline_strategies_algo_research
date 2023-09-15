6841c5aac75848fd4b20f85bf90ae0d9f95115c6
Ruzil357 <rushilgupta4@gmail.com>
2023-09-15 14:55:35 +0530

    Added ce_and_pe_buying

---
+++ src/strategies/ce_and_pe_buying.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import math
+import pandas as pd
+from datetime import datetime
+
+from strategies.base_strategy import BaseStrategy
+from constants import TransactionSide
+
+import utils
+
+
+class _220Entry(BaseStrategy):
+    def __init__(self, symbol, data: pd.DataFrame = pd.DataFrame.empty):
+        super().__init__(symbol=symbol, data=data)
+
+    def _run(self) -> list:
+        buy_row = self._data[
+            self._data["time"].dt.time == datetime(1970, 1, 1, 9, 15).time()
+        ].iloc[0]
+        buy_price = buy_row["open"]
+        quantity = utils.get_quantity(buy_price, self._capital)
+
+        sell_row = self._data[
+            self._data["time"].dt.time == datetime(1970, 1, 1, 10, 0).time()
+        ].iloc[0]
+        sell_price = sell_row["close"]
+
+        return [
+            {
+                "symbol": self._symbol,
+                "timestamp": buy_row["time"],
+                "price": buy_price,
+                "quantity": quantity,
+                "side": TransactionSide.BUY,
+            },
+            {
+                "symbol": self._symbol,
+                "timestamp": sell_row["time"],
+                "price": sell_price,
+                "quantity": quantity,
+                "side": TransactionSide.SELL,
+            },
+        ]
